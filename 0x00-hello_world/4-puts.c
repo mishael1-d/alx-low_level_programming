e5c2b6fe667202d852126819e839b0ddf1ed1f8a
Mishael <mishaeldada@gmail.com>
2022-03-10 05:49:05 -0800

    task 4 solved

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
 	/**
-	 * main -Entry point
+	 * main = Entry point
 	 *
 	 * Return: Always 0 (success)
 	 */
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 }
