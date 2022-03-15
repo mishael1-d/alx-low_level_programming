44ebf3dd0ddecc5bc5a81162959c43217be0ac8c
Mishael <mishaeldada@gmail.com>
2022-03-15 04:01:15 -0700

    added all task

--- 0x02-function_nested_loops/4-isaplha.c
+++ 0x02-function_nested_loops/4-isaplha.c
@@ -1,28 +1,13 @@
-#include <stdio.h>
-#include <conio.h>
-#include <ctype.h>
+#include "main.h"
 
 
 /**
  * _isalpha - Shows 1 if the input is a upper case character,
- * 
+ * @c: the character to be checked
  * Return: 1 for upper case, 0 otherwise
  */
 
 int _islower(int c)
 {
-    return (c >= 'A' && c <= 'Z');
-}
-
-int main(void)
-{
-    int r;
-    r = _islower('h');
-    putchar(r + '0');
-    r = _islower('H');
-    putchar(r + '0');
-    r = _islower(100);
-    putchar(r + '0');
-
-    return (0);
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
--- 0x02-function_nested_loops/5-sign.c
+++ 0x02-function_nested_loops/5-sign.c
@@ -1,48 +1,28 @@
-#include <stdio.h>
-#include <conio.h>
-#include <ctype.h>
+#include "main.h"
 
+/**
+ * print_sign - print the sign of a number
+ * @n: the int to check
+ * Return: 1 and prints + if n is greater than zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
+ */
 
 int print_sign(int n)
 {
-    if (n > 0)
-    {
-        putchar('+');
-        return 1;
-    }
-    else if (n == 0)
-    {
-        putchar(48);
-        return 0;
-    }
-    else if (n < 1)
-    {
-        putchar('-');
-    }
-        return -1;
-}
-
-int main(void)
-{
-    int r;
-    r = print_sign(98);
-    putchar(',');
-    putchar(' ');
-    putchar(r + '0');
-    putchar('\n');
-    
-    r = print_sign(0);
-    putchar(',');
-    putchar(' ');
-    putchar(r + '0');
-    putchar('\n');
-    
-    r = print_sign(-1);
-    putchar(',');
-    putchar(' ');
-    putchar(r + '0');
-    putchar('\n');
-    
-
-    return (0);
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar(48);
+		return (0);
+	}
+	else if (n < 1)
+	{
+		putchar('-');
+	}
+	return (-1);
 }
