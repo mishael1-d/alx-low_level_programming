18dec63713dfa35537f96ff736c1112fea2cfeca
Mishael <mishaeldada@gmail.com>
2022-03-21 07:51:01 -0700

    added task 3-5

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - pribts a string to stdout
+ * @str: points to the string to print
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
