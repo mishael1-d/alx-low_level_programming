4682192be0822b001dc50588e8c68df06a70458b
Mishael <mishaeldada@gmail.com>
2022-03-31 13:21:28 -0700

    fixed eror

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 
 int bandersnatch(char *s1, char *s2);
 char *move(char *s2);
@@ -80,4 +80,30 @@ int bandersnatch(char *s1, char *s2)
 	 * if we reached the end of s1, return 0
 	 * if chars are equal, return the return value of wildcmp()
 	 * increment s1 by 1, not s2
+	 */
+	if (*s1 == '\0')
+		return (0);
+	if (*s1 == *s2)
+		return (wildcmp(s1, s2));
+	return (bandersnatch(s1 + 1, s2));
+}
 
+/**
+ * *move - moves the current char past the *
+ * @s2: string to iterate over
+ *
+ * Return: the address of the character after the *
+ */
+char *move(char *s2)
+{
+	/**
+	 * if the current char is a *
+	 * increment s2 by 1
+	 * else return the address of
+	 * the first char past all *
+	 */
+	if (*s2 == '*')
+		return (move(s2 + 1));
+	else
+		return (s2);
+}
